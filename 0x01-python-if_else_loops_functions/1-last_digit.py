905ef8da0355b8c53dcc7ea380370b9cfc56449b
mayii527 <3434@holbertonschool.com>
2021-09-07 12:47:58 -0500

    reemplazo de number por last_digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ if number < 0:
     last_digit = number % -10
 else:
     last_digit = number % 10
-if number > 5:
+if last_digit > 5:
     print('Last digit of {:d} is {:d} and is greater than 5'.format(number, last_digit))
-elif number == 0:
+elif last_digit == 0:
     print('Last digit of {:d} is {:d} and is zero'.format(number, last_digit))
 else:
     print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(number, last_digit))
