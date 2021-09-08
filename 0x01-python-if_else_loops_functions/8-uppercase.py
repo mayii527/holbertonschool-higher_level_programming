d25bd895593e31c1b6fcbbf73ce1a64f3981244d
mayii527 <3434@holbertonschool.com>
2021-09-07 22:10:01 -0500

    add point 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin//python3
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 65 and ord(c) <= 90:
+            c = chr(ord(c))
+            print("{}".format(c),end="")
+            print("")
