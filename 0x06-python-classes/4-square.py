973879e3c899c0d91a1fd9be137e54b4e28ccf01
mayii527 <3434@holbertonschool.com>
2021-09-29 16:51:52 -0500

    add point 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+class Square:
+    """constructor"""
+    @property
+    def size(self):
+        """return size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """get the size"""
+        self.__size = value
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
+
+    def __init__(self, size=0):
+        """Args:
+        size: the size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
