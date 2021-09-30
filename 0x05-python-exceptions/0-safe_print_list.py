feb88659dc51bcc21541f00c352a4cd91e0419ee
mayii527 <3434@holbertonschool.com>
2021-09-29 22:53:07 -0500

    fix identation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     element = 0
     try:
-       for i in range(x):
-           print("{}".format(my_list[i]), end="")
-           element = element + 1
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            element = element + 1
     except:
         pass
     print()
