c9d437da73e2e08af89db1e4a2cd0a2b9ef5ce84
mayii527 <3434@holbertonschool.com>
2021-10-07 21:40:05 -0500

    add point 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a function that prints a
+square with the character #.
+size must be an integer.
+"""
+def print_square(size):
+    """prints a square with the character #"""
+
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for row in range(size):
+        for col in range(size):
+            print("#", end="")
+        print()
