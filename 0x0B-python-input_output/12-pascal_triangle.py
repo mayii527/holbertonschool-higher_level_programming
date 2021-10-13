7da0a73eab5322a74f86cba243193e8e49c821ba
mayii527 <3434@holbertonschool.com>
2021-10-12 23:59:55 -0500

    add

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""that returns a list of lists
+of integers representing
+the Pascal’s triangle of n:
+"""
+
+
+def pascal_triangle(n):
+    """representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
