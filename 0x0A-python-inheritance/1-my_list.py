f83d8f2b97e0ab62eb25340f6bfc2c75f71532d6
mayii527 <3434@holbertonschool.com>
2021-10-13 13:35:49 -0500

    add point 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/puthon3
+"""
+created a claas named MyList
+that inherits from list
+"""
+
+
+class MyList(list):
+    """A class named MyList"""
+
+    def print_sorted(self):
+        """prints the list, but sorted"""
+        print(sorted(self))
