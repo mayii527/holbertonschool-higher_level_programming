357d24c2ccf80901f051faf6ba7a3c5b06f64010
mayii527 <3434@holbertonschool.com>
2021-10-12 23:37:18 -0500

    add poin 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a class Student
+"""
+
+
+class Student:
+    """attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            dictio = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dictio[i] = self.__dict__[i]
+            return dictio
+        else:
+            return self.__dict__
