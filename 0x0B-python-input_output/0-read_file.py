2ec98c57e481a9aff7cf84395d3524e409e9e0bf
mayii527 <3434@holbertonschool.com>
2021-10-12 20:17:01 -0500

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ function that reads a text file
 
 
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             print(line, end="")
