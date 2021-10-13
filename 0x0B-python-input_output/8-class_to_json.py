e3f0162340f2e705c5178e4d673d0281fb6e2baf
mayii527 <3434@holbertonschool.com>
2021-10-12 23:19:23 -0500

    add point 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Write a function that returns the dictionary
+description with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """return the description of dictionary"""
+
+    return obj.__dict__
