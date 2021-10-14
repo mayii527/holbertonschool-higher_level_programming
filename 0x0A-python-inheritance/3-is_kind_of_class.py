3b47145271802c76c307624bbabc93e67a8a62b5
mayii527 <3434@holbertonschool.com>
2021-10-13 21:37:20 -0500

    add point 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+define class def is_kind_of_class(obj, a_class)
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return tru or false of a class that inherited"""
+
+    return isinstance(obj, a_class)
