42094ea3bbe791e8d2e3aa8222173f4a5af5bf5a
mayii527 <3434@holbertonschool.com>
2021-11-16 22:56:55 -0500

    fixs

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,4 +5,9 @@ name VARCHAR(256)
 score INT
 );
 -- Your script should create these records:
-INSERT INTO`second_table`(`id`, `name`, `score`) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+INSERT INTO`second_table`(`id`, `name`, `score`)
+VALUES
+(1, "John", 10), 
+(2, "Alex", 3), 
+(3, "Bob", 14), 
+(4, "George", 8);
