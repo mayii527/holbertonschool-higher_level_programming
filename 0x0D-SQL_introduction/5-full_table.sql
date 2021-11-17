d1c65e57d934ae023f9c5e339a0d6df22c951907
mayii527 <3434@holbertonschool.com>
2021-11-16 22:14:29 -0500

    try with DESC

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
---  that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL serve.
+-- that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL serve.
 
-SHOW FULL first_table;
+DESC first_table;
