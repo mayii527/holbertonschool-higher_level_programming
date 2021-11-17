e4c07aca54a91d7064d651547bcebf9707bb5ceb
mayii527 <3434@holbertonschool.com>
2021-11-17 16:28:14 -0500

    fixs

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
--- 
-CREATE USER IF NOT EXISTS ALL PRIVILEGES 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- creates the MySQL server user user_0d_1.
+-- ser_0d_1 should have all privileges on your MySQL server
+-- The user_0d_1 password should be set to user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
