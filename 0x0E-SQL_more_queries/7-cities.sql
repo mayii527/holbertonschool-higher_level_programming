ad6ccd2213c2c1a6b40446c232af76ff3e874115
mayii527 <3434@holbertonschool.com>
2021-11-17 18:10:58 -0500

    fixs

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-id INT UNIQUE AUTO_INCREMENT PRIMARY KEY ,
-state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+id INT AUTO_INCREMENT PRIMARY KEY,
+state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
 name VARCHAR(256) NOT NULL
 );
