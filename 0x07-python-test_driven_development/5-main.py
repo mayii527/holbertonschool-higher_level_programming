cce0dcf2b9922e149d313127d6f7e786ce5919df
mayii527 <3434@holbertonschool.com>
2021-11-18 15:29:10 -0500

    deletes main.py files

--- 0x07-python-test_driven_development/5-main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-text_indentation = __import__('5-text_indentation').text_indentation
-
-text_indentation("I love travel. I can choose different kinds of vehicles: car, airplane, metro, train, motorcycle. Which one do you prefer? I prefer train!")
