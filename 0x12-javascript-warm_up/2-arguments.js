96ac083a650d1f48103b830d630f5af5af332fc3
mayii527 <3434@holbertonschool.com>
2021-12-26 22:08:10 -0500

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
- // prints a message depending of the number of arguments.
+// prints a message depending of the number of arguments.
 
- if (process.argv.length === 2) {
-	console.log('No argument');
-} else if (process.argv.length === 3) {
-	console.log('Argument found');
-} else {
-	console.log('Arguments found');
-}
+if (process.argv.length === 2) {
+    console.log('No argument')
+  } else if (process.argv.length === 3) {
+    console.log('Argument found')
+  } else {
+    console.log('Arguments found')
+  }
+  
