7ccbe4d448bcd5ef60852bc3a4f77cb66240a54c
mayii527 <3434@holbertonschool.com>
2022-01-31 09:44:25 -0500

    .

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Write a Python script that fetches
-https://intranet.hbtn.io/status
+fetches https://intranet.hbtn.io/status
 """
 import urllib.request
 
@@ -10,3 +9,7 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', (html.decode('utf-8')))
