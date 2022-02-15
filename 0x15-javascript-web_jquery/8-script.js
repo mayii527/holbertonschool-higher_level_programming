f6430d96109886740ee4dc4728fb5d7027416c51
mayii527 <3434@holbertonschool.com>
2022-02-15 17:22:01 -0500

    add task 8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,7 @@
-/**/
+/*fetches and lists the title for all movies by using this
+URL: https://swapi-api.hbtn.io/api/films/?format=json*/
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', 
+function (resp){
+    for(var i = 0; i < resp.results.length; i++)
+    $('#list_movies').append('<li>'+resp.results[i].title+'</li>')
+});
