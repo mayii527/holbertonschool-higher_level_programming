139def04ef55d43bcace1703971a25be9b47836a
mayii527 <3434@holbertonschool.com>
2022-02-15 09:47:15 -0500

    fix function name

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,5 @@
 //adds the class red to the <header> element
 //when the user clicks on the tag DIV#red_header
 $('#red_header').click(function (){
-    $('header').addCalss('red');
+    $('header').addClass('red');
 });
