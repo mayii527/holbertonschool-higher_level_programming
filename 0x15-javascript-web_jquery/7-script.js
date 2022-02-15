6ed44a8a8d673876dd191d5e94b8907be41199e9
mayii527 <3434@holbertonschool.com>
2022-02-15 10:09:09 -0500

    fix

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,6 @@
 //fetches the character name from this 
 //URL: https://swapi-api.hbtn.io/api/people/5/?format=json
-$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(resp){
-    $('#character').text(resp.name);
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json',
+function(resp){
+    $('#character').text(resp.name +' '+ resp.gender);
 });
--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,6 @@
 //fetches and displays the value of hello
 //from that fetch in the HTML tag DIV#hello.
-$('header').getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function(resp){
+$('header').getJSON('https://fourtonfish.com/hellosalut/?lang=fr',
+function(resp){
     $('#hello').text(resp.hello);
 });
